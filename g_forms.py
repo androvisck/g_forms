a174a9b910c929c06ff3e7e3b4ef5763ef7f190c
André França <39207342+androvisck@users.noreply.github.com>
2020-02-03 20:52:14 -0300

    Atualização prévia
    Atualização antes de trocar o formulário.

--- g_forms.py
+++ g_forms.py
@@ -16,15 +16,18 @@ from openpyxl import load_workbook
 wb = load_workbook('test.xlsx') # abrindo o Workbook test.xlsx
 ws = wb['Planilha1'] # selecionando a planilha 'Planilha1' dentro do Workbook test.xlsx
 linha=0
+
 for line in ws:  # iterando em todas as linhas da 'Plan1'
     #print(line[0]) # print a primeira célula da linha
     linha = linha + 1
     #print(linha)
 
+"""
 # Utililizando em índices o nome das células como em um dicionário
-c1 = ws['C1']
+a1 = ws['A1']
 # Imprime o valor da célula C1
-print(c1.value)
+print(a1.value)
+"""
 
 # Escrita no formulário online
 url = 'https://docs.google.com/forms/d/e/1FAIpQLScBRmhzR0d7u7NSnSlTf66E-6NziilDHIo28u8R4gh_5MizXw/formResponse'
@@ -33,7 +36,7 @@ for x in range(2,linha+1):
     Observ='Usuário'
     Tarefa='Primeira'
     Item='10'
-    Rot='Produto A'
+    Rot= ws['A'+str(x)].value
     Escolha='Opção 1'
     form_data = {'entry.366340186': Observ, 'entry.443668215': Tarefa, 'entry.22026032': Item, 'entry.371113979': Rot, 'entry.489632448': Escolha, 'draftResponse':[], 'pageHistory':0}
 
