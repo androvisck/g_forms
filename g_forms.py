73d19bc212cd0dde11e2440b51c7180a71c43fe5
André França <39207342+androvisck@users.noreply.github.com>
2020-02-04 13:37:00 -0300

    Atualização
    Antes de inserir a interface visual.

--- g_forms.py
+++ g_forms.py
@@ -1,6 +1,6 @@
 """
 Script relativo ao teste de automação do candidato André França
-Realiza preenchimento de um formulário do Google.
+Este scriot realiza o preenchimento de um formulário do Google.
 Os dados estão contidos em uma planilha de excel.
 
 
@@ -13,14 +13,14 @@ import requests
 from openpyxl import load_workbook
 
 # Leitura da planilha
-wb = load_workbook('test.xlsx') # abrindo o Workbook test.xlsx
-ws = wb['Planilha1'] # selecionando a planilha 'Planilha1' dentro do Workbook test.xlsx
-linha=0
+wb = load_workbook('test.xlsx')  # abrindo o Workbook test.xlsx
+ws = wb['Planilha1']  # selecionando a planilha 'Planilha1' dentro do Workbook test.xlsx
+linha = 0
 
 for line in ws:  # iterando em todas as linhas da 'Plan1'
-    #print(line[0]) # print a primeira célula da linha
+    # print(line[0]) # print a primeira célula da linha
     linha = linha + 1
-    #print(linha)
+    # print(linha) # print a qtdd de linhas
 
 """
 # Utililizando em índices o nome das células como em um dicionário
@@ -30,15 +30,20 @@ print(a1.value)
 """
 
 # Escrita no formulário online
-url = 'https://docs.google.com/forms/d/e/1FAIpQLScBRmhzR0d7u7NSnSlTf66E-6NziilDHIo28u8R4gh_5MizXw/formResponse'
+### OBSERVAÇÃO ~> retire '/viewform' e add '/formResponse' ao fim do link ###
 
-for x in range(2,linha+1):
-    Observ='Usuário'
-    Tarefa='Primeira'
-    Item='10'
-    Rot= ws['A'+str(x)].value
-    Escolha='Opção 1'
-    form_data = {'entry.366340186': Observ, 'entry.443668215': Tarefa, 'entry.22026032': Item, 'entry.371113979': Rot, 'entry.489632448': Escolha, 'draftResponse':[], 'pageHistory':0}
+url = "https://docs.google.com/forms/d/e/1FAIpQLSd3dOHHwAQ7JtxBc-Yyfu2B9jBT834Z6OLv_STMPm9qqYdyjg/formResponse"
 
-    user_agent = {'Referer':url,'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
+# Laço for com número de iterações = número de linhas do documento
+for x in range(1, linha + 1):
+    nome = 'André José de França'
+    Item = ws['A' + str(x)].value
+    CEP = ws['B' + str(x)].value
+    Endereco = ws['C' + str(x)].value
+    Complemento = ws['D' + str(x)].value
+    form_data = {'entry.366340186': nome, 'entry.1613519622': Item, 'entry.2045809580': CEP,
+                 'entry.1334556551': Endereco, 'entry.1114882091': Complemento, 'draftResponse': [], 'pageHistory': 0}
+
+    user_agent = {'Referer': url, 'User-Agent':"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                               "Chrome/28.0.1500.52 Safari/537.36"}
     r = requests.post(url, data=form_data, headers=user_agent)
